838b201e49699f40f0a4832aa102191caf7910f3
Akashae98 <carolvr8@gmail.com>
2025-06-13 11:29:18 +0200

    pecera

--- src/main/java/com/mycompany/animacionpecera/Animation.java
+++ src/main/java/com/mycompany/animacionpecera/Animation.java
@@ -31,7 +31,7 @@ public class Animation extends Application {
     @Override
     public void start(Stage stage) {
         // Canvas of 600x400 pixels habilitates to draw
-        Canvas canvas = new Canvas(600, 400);
+        Canvas canvas = new Canvas(FishTank.CANVAS_WIDTH,FishTank.CANVAS_HEIGH);
         gc = canvas.getGraphicsContext2D(); //creates graphicContext in the Canvas
         fishTank = new FishTank(); //instance of FishTank
         
--- src/main/java/com/mycompany/animacionpecera/FishTank.java
+++ src/main/java/com/mycompany/animacionpecera/FishTank.java
@@ -11,12 +11,21 @@ import javafx.scene.canvas.GraphicsContext;
  This class manages a list of fishes.
  */
 public class FishTank {
-    // list that contains the fishes
+    public static int CANVAS_WIDTH = 600;
+    public static int CANVAS_HEIGH = 400;
     private ArrayList<Fish> fishesList;
 
     // Constructor: intitialize array list
     public FishTank() {
         this.fishesList = new ArrayList<>();
+    }
+
+    public static int getCANVAS_WIDTH() {
+        return CANVAS_WIDTH;
+    }
+
+    public static int getCANVAS_HEIGH() {
+        return CANVAS_HEIGH;
     }
 
     // Adds a fish in the array and creates a fish in a position
