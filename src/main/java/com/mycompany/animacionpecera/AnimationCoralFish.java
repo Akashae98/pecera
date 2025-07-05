ab5868ebd395e99e29b3111dd941f0def5dcc6fa
User <User@DESKTOP-NKNPNP1>
2025-07-05 10:25:33 +0200

    cambios pr

--- src/main/java/com/mycompany/animacionpecera/AnimationCoralFish.java
+++ src/main/java/com/mycompany/animacionpecera/AnimationCoralFish.java
@@ -1,3 +1,4 @@
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -6,66 +7,63 @@ package com.mycompany.animacionpecera;
 
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
 
 /**
  *
  * @author User
  */
 public class AnimationCoralFish extends Animation {
-    
+
     private Image image;
 
     public AnimationCoralFish(double size) {
         super(size);
+        this.image = new Image(getClass().getResourceAsStream("/Images/sketchPezCoral.png"));
     }
-    
+
     @Override
     public BoundingBox getBoundingBox(Position position) {
-        //the base of the bodyfish 
-        double bodyWidth = 36 * size;
-        //the height of the bodyfish
-        double bodyHeight = 20 * size;
-        //the height of the fishtail
-        double tailHeight = 9 * size;
+        double bodyWidth = 90 * size;
+        double bodyHeight = 90 * size;
+        double tail_displacement = 33 * size;
+        double left_displacement = 4 * size;
+        double top_displacement = 4 * size;
 
         Position topLeft = new Position(
-                position.x,
-                position.y - (bodyHeight / 2)
+                position.x + left_displacement,
+                position.y + top_displacement
         );
 
         Position topRight = new Position(
-                position.x + bodyWidth,
-                position.y - (bodyHeight / 2)
+                position.x + bodyWidth + tail_displacement,
+                position.y + top_displacement
         );
 
         Position bottomRight = new Position(
-                position.x + bodyWidth,
-                position.y + (bodyHeight / 2) + tailHeight
+                position.x + bodyWidth + tail_displacement,
+                position.y + bodyHeight + tail_displacement
         );
 
         Position bottomLeft = new Position(
-                position.x,
-                position.y + (bodyHeight / 2) + tailHeight
+                position.x + left_displacement,
+                position.y + bodyHeight + tail_displacement
         );
 
         return new BoundingBox(topLeft, topRight, bottomRight, bottomLeft);
     }
+
     @Override
-    public void draw(GraphicsContext gc, double x, double y) {
-         chargeDraw();
-            double ancho = image.getWidth() * size;
-            double alto = image.getHeight() * size;
-            gc.drawImage(image, x, y, ancho, alto);
+    public void draw(GraphicsContext gc, Position pos) {
+
+        double width = image.getWidth() * size;
+        double height = image.getHeight() * size;
+
+        gc.drawImage(image, pos.x, pos.y, width, height);
+        BoundingBox boundingBox = getBoundingBox(pos);
+        drawBoundingBox(gc, boundingBox, Color.WHITE);
+        gc.strokeText("o", pos.x, pos.y);
+
     }
-    
-    
-     private void chargeDraw() {
-        try {
-            this.image = new Image(getClass().getResourceAsStream("/Images/sketchPezCoral.png"));
-        } catch (Exception e) {
-            System.err.println("Error al cargar imagen de pez coral: " + e.getMessage());
-        }
-    }
-     
-     
+
 }
