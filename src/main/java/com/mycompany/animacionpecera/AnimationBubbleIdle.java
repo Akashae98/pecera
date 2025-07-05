cc7499a7040138e3b3f3baf91ef15020da9b3a41
User <User@DESKTOP-NKNPNP1>
2025-07-05 10:25:21 +0200

    cambios pr

--- src/main/java/com/mycompany/animacionpecera/AnimationBubbleIdle.java
+++ src/main/java/com/mycompany/animacionpecera/AnimationBubbleIdle.java
@@ -20,27 +20,28 @@ public class AnimationBubbleIdle extends Animation {
     @Override
     public BoundingBox getBoundingBox(Position position) {
 
-        double Width = 10 * size;
-        double Height = 10 * size;
+        double Width = 1 * size;
+        double Height = 0.5 * size;
+        double extra_bottom = 0.25 * size;
 
         Position topLeft = new Position(
                 position.x,
-                position.y - (Height / 2)
+                position.y - (Height / 2) + extra_bottom
         );
 
         Position topRight = new Position(
                 position.x + Width,
-                position.y - (Height / 2)
+                position.y - (Height / 2) + extra_bottom
         );
 
         Position bottomRight = new Position(
                 position.x + Width,
-                position.y + (Height / 2) + Height
+                position.y + (Height / 2) + Height + extra_bottom
         );
 
         Position bottomLeft = new Position(
                 position.x,
-                position.y + (Height / 2) + Height
+                position.y + (Height / 2) + Height + extra_bottom
         );
 
         return new BoundingBox(topLeft, topRight, bottomRight, bottomLeft);
@@ -52,6 +53,10 @@ public class AnimationBubbleIdle extends Animation {
         gc.fillOval(position.x, position.y, this.size, size);//fills with color the inside of bubble
         gc.setStroke(Color.rgb(255, 255, 255, 0.5));//white color for the bubble edge
         gc.strokeOval(position.x, position.y, size, size);//fills with color
+        BoundingBox boundingBox = getBoundingBox(position);
+        drawBoundingBox(gc, boundingBox, Color.WHITE);
+        
+        gc.strokeText("o", position.x, position.y);
     }
 
 }
