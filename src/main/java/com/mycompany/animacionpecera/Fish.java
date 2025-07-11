5da8411cf4ad1616561a4f821020826febf20bf4
Akashae98 <carolvr8@gmail.com>
2025-07-11 15:37:18 +0200

    Abstract class movement #12 (#19)
    * Merge #13_Abstract_class_Animation_(to_draw) into Feature-Red_fish_realistic_drawing
    
    # Conflicts:
    #	src/main/java/com/mycompany/animacionpecera/Fish.java
    
    * Merge #13_Abstract_class_Animation_(to_draw) into Feature-Red_fish_realistic_drawing
    
    # Conflicts:
    #	src/main/java/com/mycompany/animacionpecera/Fish.java
    
    * Merge #13_Abstract_class_Animation_(to_draw) into Feature-Red_fish_realistic_drawing
    
    # Conflicts:
    #	src/main/java/com/mycompany/animacionpecera/Fish.java
    
    * Merge #13_Abstract_class_Animation_(to_draw) into Feature-Red_fish_realistic_drawing
    
    # Conflicts:
    #	src/main/java/com/mycompany/animacionpecera/Fish.java
    
    * clase abstracta modificada
    
    * cambios abstracta
    
    * clase abstracta modificada
    cambio nombre del main
    x e y fuera de la abstracta, se pasan al metodo draw
    
    * new class AnimationBubbleidle,
    new class fo random color,
    method randomPoint in Fishtank
    
    * more bubbles
    
    * more bubbles
    
    * cambios en rebound, fishtank, fish, size..
    
    * cambios getrandompoint
    
    * cambios getrandompoint
    
    * cambios Color en constructor fish, randomcolor se usa al instanciar Fish
    
    * nombre Position para objeto pair
    
    * nombre Position ora objeto pair
    
    * Merge #13_Abstract_class_Animation_(to_draw) into bug_#5_fish_passes_out_of_fishtank
    
    # Conflicts:
    #	src/main/java/com/mycompany/animacionpecera/AnimationFishIdle.java
    #	src/main/java/com/mycompany/animacionpecera/Fish.java
    #	src/main/java/com/mycompany/animacionpecera/FishTank.java
    
    * new class position
    
    * new class position
    
    * new class position
    
    * cambios en metodos movimiento por objeto position
    
    * Objeto position en constructores
    
    * cambios reboun, nueva clase boundinbox
    
    * cambios en rebound, nueva clase boundingbox
    
    * valores rebote ajuste
    
    * Boundingbox simplified
    
    * Boundingbox simplified, comments
    
    * Boundingbox simplified, comments
    
    * Boundingbox simplified, comments
    
    * Boundingbox simplified, comments
    
    * comments
    
    * added variable heightFishTail
    
    * limite no añadir en bordes pecera
    
    * method getBoundingBox in Animation,
    integrated with fishiddle
    
    * changes boundingbox, 4 points
    changed method boundingbox
    
    * points in boundingbox
    
    * cambios pr
    
    * cambios en position: metodo para sumar o restar posicion + getters.
    cambio en metodo rebound, el rebote invierte la dieccion de forma constante.
    nuevo metodo teletransporte, controla que nunca aparezca un borde del box fuera
    
    * coralfish sin atributo imagen
    
    * cambios pr
    
    * cambios pr
    
    * calculos boundingbox burbuja actualizado
    metodo drawBoundingbox se hereda de clase animation
    
    * calculos boundingbox burbuja actualizado
    metodo drawBoundingbox se hereda de clase animation
    
    * cambios pr
    
    * cambios pr
    
    * cambios pr
    
    * clase abstracta movement, linear movement y movementrebound,
    + clase direction.
    
    * cambios en reboundmovement, +pez burbuja?
    
    * espaciado
    
    * cambios pr, nueva clase SceneObject
    pendiente revisar
    
    * cambios pr, nueva clase SceneObject
    eliminado un metodo de getboundingbox
    
    * se usa el boundingbox de canvas en rebound movement y loop out of bounds
    
    * se usa el boundingbox de canvas en rebound movement y loop out of bounds
    1 comment changed
    
    * offset fuera
    
    * Revert "offset fuera"
    
    This reverts commit 9eb59aa99ba083e131a1ffc268b7be9ffaa5d2d4.
    
    * offset fuera
    
    * in method loop one line changed
    
    ---------
    
    Co-authored-by: carol <carol@meleys>
    Co-authored-by: User <User@DESKTOP-NKNPNP1>

--- src/main/java/com/mycompany/animacionpecera/Fish.java
+++ src/main/java/com/mycompany/animacionpecera/Fish.java
@@ -4,50 +4,15 @@
  */
 package com.mycompany.animacionpecera;
 
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.paint.Color;
-
 /**
- * This class let us create fishes with position, velocity and random colors,
- * etc. Fishes can be created with a fishfin or with scales depending on whether
- * the boolean hasFishFin its true or false.
+ * This class manages the position, movement and animation of different
+ * fishes...
  *
  */
-public class Fish {
+public class Fish extends SceneObject {
 
-    private double dx, dy;    //Direction for movement
-    private Movement movement;
-    private final Animation anim;
-    private Position position;
-    public BoundingBox boundingBox;
-
-    public Fish(Position pos, Animation animation) {
-        this.position = pos;
-        this.dx = Math.random() * 2 - 1; //Aleatory movement between -1 and 1 in x-axis
-        this.dy = Math.random() * 2 - 1;// Same in y-axis
-        this.movement = new Movement();
-        this.anim = animation;
-        this.boundingBox = anim.getBoundingBox(position);
-    }
-
-    //Method of movement
-    public void move() {
-        // updates postion
-        position = position.displacement(dx, dy);
-
-        //updates boundingbox position
-        boundingBox = anim.getBoundingBox(position);
-
-        //handles rebounding and changes direction
-        double[] newDirect = movement.rebound(boundingBox, dx, dy);
-        this.dx = newDirect[0];
-        this.dy = newDirect[1];
-
-    }
-
-    //Method for drawing
-    public void draw(GraphicsContext gc) {
-        anim.draw(gc, position);
+    public Fish(Position position, Movement movementType, Animation animationType) {
+        super(position, animationType, movementType);
 
     }
 
---
+++ src/main/java/com/mycompany/animacionpecera/LinearMovement.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.animacionpecera;
+
+/**
+ *
+ * @author carol
+ */
+public class LinearMovement extends Movement {
+
+    private Direction direction;
+
+    public LinearMovement(Direction direction) {
+        this.direction = direction;
+    }
+
+
+    @Override
+    public Position nextPosition(SceneObject current) {
+        Position currentPos = current.getPosition();
+        return currentPos.displacement(direction.dx(), direction.dy());
+    }
+     public Direction getDirection() {
+        return direction;
+    }
+
+    public void setDirection(Direction direction) {
+        this.direction = direction;
+    }
+    
+}
